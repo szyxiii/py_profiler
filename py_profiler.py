3772c30b6b0bfef2a2926eb5126a7288c9bb9626
szyxiii <x7174nx@gmail.com>
2021-01-19 20:33:13 +0300

    Update py_profiler.py

--- py_profiler.py
+++ py_profiler.py
@@ -1,24 +1,35 @@
 def profile(iterations, runs=1, iter_results=True):
+    '''Profile function
+    
+    :param iterations: iterations count
+    :type iterations: int
+    
+    :param runs: number of runs
+    :type runs: int
+    
+    :param iter_results: print results for each iteration
+    :type iter_results: bool
+    '''
     def body(function):
         def inner(*args):
             results = []
-            fact = 0
+            fresult = None
 
             for _ in range(runs):
                 start = time.time()
 
                 # Function calling
                 for _ in range(iterations):
-                    fact = function(*args)
+                    fresult = function(*args)
 
                 result = time.time() - start
                 results.append(result)
 
                 if iter_results:
-                    print("[Profiling:{}({}) = {}] x{} iters: {:.6f} ms".format(function.__name__, ', '.join(map(str, args)), fact, iterations, result))
+                    print("[Profiling:{}({}) = {}] x{} iters: {:.6f} ms".format(function.__name__, ', '.join(map(str, args)), fresult, iterations, result))
 
             if runs > 1:
-                print("[Profiling Average:{}({}) = {}] x{} iters, x{} runs: {:.6f} ms".format(function.__name__, ', '.join(map(str, args)), fact, iterations, runs, sum(results) / len(results)))
+                print("[Profiling Average:{}({}) = {}] x{} iters, x{} runs: {:.6f} ms".format(function.__name__, ', '.join(map(str, args)), fresult, iterations, runs, sum(results) / len(results)))
 
         return inner
     return body
